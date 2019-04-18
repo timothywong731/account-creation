72e9114af1ecdf4149a43b0f5d1f3e57aa3d53a5
Timothy Wong <timothy.wong@hotmail.co.uk>
2019-04-18 12:47:11 +0000

    Updated for CentOS Changed to HTML output

--- generate_users.R
+++ generate_users.R
@@ -1,16 +1,23 @@
 library(dplyr)
 library(readr)
+library(stringr)
 
-# myUsers <- tibble(user = paste0("user",stringr::str_pad(1:200,3,"left","0"))) %>%
-#   rowwise() %>%
-#   mutate(password = paste0(sample(c(LETTERS,letters,0:9), 6),collapse = ""))
-# 
-# myUsers %>%
-#   write_csv("users.txt")
+# Generate user name and password
+myUsers <- tibble(user = paste0("user",str_pad(1:100,3, "left", "0"))) %>%
+  rowwise() %>%
+  mutate(password = c(sample(LETTERS, 3), 
+                      sample(letters, 3),
+                      sample(0:9, 3),
+                      sample(c("!", "_", "~", "@","#", "$", "%", "^", "*", "+"), 1)) %>% 
+           sample(10) %>% 
+           paste0(collapse = ""))
 
-myUsers <- read_csv("users.txt")
+# Save the password as a flat file
+myUsers %>%
+  write_csv("users.csv")
 
-
+# Read the user/password file
+myUsers <- read_csv("users.csv")
 
 # Write the commands for creating new users -------------------------------
 
@@ -22,6 +29,8 @@ for (i in 1:nrow(myUsers)) {
   write_lines(paste0("echo -e \"", pwd,"\\n",pwd, "\" | passwd ", usr), myCmd, append = TRUE)
   write_lines(paste0("mkhomedir_helper ", usr), myCmd, append = TRUE)
   if (i <=10) {
-    write_lines(paste0("usermod -aG sudo ", usr), myCmd, append =TRUE)
+    # Use "wheel" for CENTOS
+    # Use "sudo" for Ubuntu
+    write_lines(paste0("usermod -aG wheel ", usr), myCmd, append =TRUE)
   }
 }
