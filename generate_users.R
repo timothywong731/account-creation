4a4da4dc900462e03b459ce8fbdff3463bbc207c
Ubuntu <wong0s@training.abdlb3ipl33ediina4qpdognrc.zx.internal.cloudapp.net>
2019-04-20 15:33:30 +0000

    Added empty lines to command file

--- generate_users.R
+++ generate_users.R
@@ -5,11 +5,9 @@ library(stringr)
 # Generate user name and password
 myUsers <- tibble(user = paste0("user",str_pad(1:100,3, "left", "0"))) %>%
   rowwise() %>%
-  mutate(password = c(sample(LETTERS, 3), 
-                      sample(letters, 3),
-                      sample(0:9, 3),
-                      sample(c("!", "_", "~", "@","#", "$", "%", "^", "*", "+"), 1)) %>% 
-           sample(10) %>% 
+  mutate(password = c(sample(c(LETTERS, letters), 6),
+                      sample(c("!", "_", "~", "@","#", "$", "^", "+"), 1),
+                      sample(0:9, 3)) %>% 
            paste0(collapse = ""))
 
 # Save the password as a flat file
@@ -21,16 +19,22 @@ myUsers <- read_csv("users.csv")
 
 # Write the commands for creating new users -------------------------------
 
-myCmd <- "cmd.txt"
+myCmd <- "cmd.sh"
+system(paste0("rm -rf ", myCmd))
+
 for (i in 1:nrow(myUsers)) {
   usr <- myUsers[i, ]$user
   pwd <- myUsers[i, ]$password
   write_lines(paste0("useradd ", usr), myCmd, append = TRUE)
+  write_lines("", myCmd, append = TRUE)
   write_lines(paste0("echo -e \"", pwd,"\\n",pwd, "\" | passwd ", usr), myCmd, append = TRUE)
+  write_lines("", myCmd, append = TRUE)
   write_lines(paste0("mkhomedir_helper ", usr), myCmd, append = TRUE)
+  write_lines("", myCmd, append = TRUE)
   if (i <=10) {
     # Use "wheel" for CENTOS
     # Use "sudo" for Ubuntu
     write_lines(paste0("usermod -aG wheel ", usr), myCmd, append =TRUE)
+    write_lines("", myCmd, append = TRUE)
   }
 }
