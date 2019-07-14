1621fcc2d0e3ef2a8503340cc2bf230c35e1d730
Timothy <timothy.wong@hotmail.co.uk>
2019-07-15 00:39:03 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -71,23 +71,17 @@ exit
 # Install R Packages
 sudo R -e "install.packages(c('abind','anytime','ape','assertthat','backports','base64enc','BH','bindr','bindrcpp','bitops','brew','broom','callr','car','carData','caTools','cellranger','cli','clipr','cloudml','cmprsk','colorspace','commonmark','config','cowplot','crayon','crosstalk','curl','data.table','DBI','dbplot','dbplyr','Deriv','desc','devtools','digest','dplyr','ellipsis','evaluate','exactRankTests','fansi','forcats','forecast','forge','fracdiff','generics','GGally','ggdendro','ggfortify','ggplot2','ggpubr','ggrepel','ggsci','ggsignif','git2r','glue','gridExtra','gtable','haven','hexbin','highr','hms','htmltools','htmlwidgets','httpuv','httr','jsonlite','keras','km.ci','KMsurv','knitr','labeling','later','lazyeval','lme4','lmtest','lubridate','magrittr','maptools','markdown','MatrixModels','maxstat','memoise','mime','minqa','modelr','munsell','mvtnorm','neuralnet','NeuralNetTools','nloptr','nycflights13','openssl','openxlsx','packrat','pbkrtest','pillar','pkgbuild','pkgconfig','pkgload','PKI','plogr','plotly','plyr','polynom','prettyunits','processx','profvis','progress','promises','pryr','ps','purrr','quadprog','quantmod','QuantPsyc','quantreg','r2d3','R6','randomForest','RApiDatetime','rappdirs','RColorBrewer','Rcpp','RcppArmadillo','RcppEigen','RCurl','readr','readxl','rematch','reprex','reshape','reshape2','reticulate','rio','RJSONIO','rlang','rmarkdown','roxygen2','rpart','rpart.plot','rprojroot','rsconnect','rstudioapi','rvest','scales','selectr','shiny','sourcetools','sp','sparklyr','SparseM','stringi','stringr','survival','survminer','survMisc','tensorflow','tfdatasets','tfestimators','tfruns','tibble','tidyr','tidyselect','tidyverse','timeDate','tinytex','tseries','tsibble','TTR','urca','utf8','vcd','viridisLite','whisker','withr','xfun','xml2','xtable','xts','yaml','zeallot','zip','zoo','ggthemes'), lib='/usr/lib/R/site-library')" 1>&2
 
+
 # Install TensorFlow
 sudo R -e "tensorflow::install_tensorflow(method = 'conda', conda = 'auto', envname = 'venv')" 1>&2
 
 
 # Configure SSL for RStudio
 sudo mkdir /etc/nginx/ssl
-
 sudo openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=UK/ST=Denial/O=Dis/CN=staines-r-training-july-2019.uksouth.cloudapp.azure.com"
-
 sudo chmod 600 /etc/nginx/ssl/nginx.key
-
 sudo systemctl restart nginx
 sudo rstudio-server restart
-
-
-# Create disk partition using all available space
-
 
 
 # Format the 1TB disk (/dev/sdc1) for /home
@@ -112,6 +106,7 @@ UUID_sdd1=$(sudo blkid | grep '^/dev/sdd1:' | grep -oE ' UUID="([a-zA-Z0-9-]*)" 
 echo UUID=$UUID_sdd1   /tmp   ext4   defaults,nofail   1   2 >> /etc/fstab
 exit
 sudo fstrim /tmp
+
 
 # Change permission for /tmp
 sudo chmod -R 1777 /tmp
