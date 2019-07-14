99453addd81e1ec1a97952997b1d1973bf082da9
Timothy <timothy.wong@hotmail.co.uk>
2019-07-14 18:33:49 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,6 @@ sudo apt install -y libxml2-dev
 sudo apt install -y texlive-full
 
 
-# Install R packages
-sudo R -e "install.packages('reticulate', lib='/usr/lib/R/site-library')" 1>&2
-
 
 # Install RStudio Server
 sudo apt install -y gdebi-core
@@ -69,5 +66,5 @@ exit
 
 
 # Install TensorFlow (R & Python)
-sudo R -e "install.packages('tensorflow', lib='/usr/lib/R/site-library')" 1>&2
+sudo R -e "install.packages(c('abind','anytime','ape','assertthat','backports','base64enc','BH','bindr','bindrcpp','bitops','brew','broom','callr','car','carData','caTools','cellranger','cli','clipr','cloudml','cmprsk','colorspace','commonmark','config','cowplot','crayon','crosstalk','curl','data.table','DBI','dbplot','dbplyr','Deriv','desc','devtools','digest','dplyr','ellipsis','evaluate','exactRankTests','fansi','forcats','forecast','forge','fracdiff','generics','GGally','ggdendro','ggfortify','ggplot2','ggpubr','ggrepel','ggsci','ggsignif','git2r','glue','gridExtra','gtable','haven','hexbin','highr','hms','htmltools','htmlwidgets','httpuv','httr','jsonlite','keras','km.ci','KMsurv','knitr','labeling','later','lazyeval','lme4','lmtest','lubridate','magrittr','maptools','markdown','MatrixModels','maxstat','memoise','mime','minqa','modelr','munsell','mvtnorm','neuralnet','NeuralNetTools','nloptr','nycflights13','openssl','openxlsx','packrat','pbkrtest','pillar','pkgbuild','pkgconfig','pkgload','PKI','plogr','plotly','plyr','polynom','prettyunits','processx','profvis','progress','promises','pryr','ps','purrr','quadprog','quantmod','QuantPsyc','quantreg','r2d3','R6','randomForest','RApiDatetime','rappdirs','RColorBrewer','Rcpp','RcppArmadillo','RcppEigen','RCurl','readr','readxl','rematch','reprex','reshape','reshape2','reticulate','rio','RJSONIO','rlang','rmarkdown','roxygen2','rpart','rpart.plot','rprojroot','rsconnect','rstudioapi','rvest','scales','selectr','shiny','sourcetools','sp','sparklyr','SparseM','stringi','stringr','survival','survminer','survMisc','tensorflow','tfdatasets','tfestimators','tfruns','tibble','tidyr','tidyselect','tidyverse','timeDate','tinytex','tseries','tsibble','TTR','urca','utf8','vcd','viridisLite','whisker','withr','xfun','xml2','xtable','xts','yaml','zeallot','zip','zoo','ggthemes'), lib='/usr/lib/R/site-library')" 1>&2
 sudo R -e "tensorflow::install_tensorflow(method = 'conda', conda = 'auto', envname = 'venv')" 1>&2
