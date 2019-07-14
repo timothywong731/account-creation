67a513055b603efe8d8229d769ef3699b5f3d827
Ubuntu <wong0s@training.1dchulygzxhe5kvggwspswwbqa.zx.internal.cloudapp.net>
2019-07-14 23:44:14 +0000

    script updated

--- generate_users.R
+++ generate_users.R
@@ -9,7 +9,7 @@ MY_CMD <- "cmd.sh"
 myUsers <- tibble(user = paste0("user",str_pad(1:100,3, "left", "0"))) %>%
   rowwise() %>%
   mutate(password = c(sample(c(LETTERS, letters), 6),
-                      sample(c("!", "_", "~", "@","#", "$", "^", "+"), 1),
+                      sample(c("!", "_", "~", "@","#", "^"), 1),
                       sample(0:9, 3)) %>% 
 paste0(collapse = ""))
 
@@ -29,15 +29,12 @@ for (i in 1:nrow(myUsers)) {
   usr <- myUsers[i, ]$user
   pwd <- myUsers[i, ]$password
   write_lines(paste0("useradd ", usr), MY_CMD, append = TRUE)
-  write_lines("", MY_CMD, append = TRUE)
   write_lines(paste0("echo -e \"", pwd,"\\n",pwd, "\" | passwd ", usr), MY_CMD, append = TRUE)
-  write_lines("", MY_CMD, append = TRUE)
   write_lines(paste0("mkhomedir_helper ", usr), MY_CMD, append = TRUE)
-  write_lines("", MY_CMD, append = TRUE)
   if (i <=10) {
     # Use "wheel" for CENTOS
     # Use "sudo" for Ubuntu
     write_lines(paste0("usermod -aG sudo ", usr), MY_CMD, append =TRUE)
-    write_lines("", MY_CMD, append = TRUE)
   }
+  write_lines("", MY_CMD, append = TRUE)
 }
