25a9cd9bb0612f33273085c67cdad253dbd143dc
Timothy <timothy.wong@hotmail.co.uk>
2019-10-10 14:17:22 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Add GPG key and R repository
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
